47c790bb18edaf9defd12ab580835a97998fddf7
vivatecpe-dot <vivatecpe@gmail.com>
2025-10-20 14:04:44 -0500

    Add files via upload

--- components/icons/DocumentIcon.tsx
+++ components/icons/DocumentIcon.tsx
@@ -11,3 +11,9 @@ export const ClipboardListIcon: React.FC = () => (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"></path>
     </svg>
 );
+
+export const DownloadIcon: React.FC = () => (
+    <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+    </svg>
+);
